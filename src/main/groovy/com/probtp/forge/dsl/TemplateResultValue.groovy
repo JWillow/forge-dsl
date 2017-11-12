6168134ed728c095ea4f3581014e3c7341983f26
JWillow <rletient@gmail.com>
2017-11-12 16:37:05 +0100

    manage append option to TemplateResultValue

--- src/main/groovy/com/probtp/forge/dsl/TemplateResultValue.groovy
+++ src/main/groovy/com/probtp/forge/dsl/TemplateResultValue.groovy
@@ -6,23 +6,35 @@ class TemplateResultValue {
 
     String value
 
-    private void write(File file) {
+    private void write(File file, boolean append) {
         if(file.isDirectory()) {
             throw new IllegalArgumentException("The destination must be a File, not a Directory")
         }
-        FileUtils.write(file, value, "UTF-8")
+        FileUtils.writeStringToFile(file, value, "UTF-8", append)
     }
 
     void rightShift(String strFile) {
-        write(new File(strFile))
+        write(new File(strFile), true)
     }
 
     void rightShift(File file) {
-        write(file)
+        write(file, true)
     }
 
     void rightShift(FileHandler handler) {
-        write(handler.file)
+        write(handler.file, true)
+    }
+
+    void rightShiftUnsigned(String strFile) {
+        write(new File(strFile), false)
+    }
+
+    void rightShiftUnsigned(File file) {
+        write(file, false)
+    }
+
+    void rightShiftUnsigned(FileHandler handler) {
+        write(handler.file, false)
     }
 
 }
